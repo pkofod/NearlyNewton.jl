70a60242824030c2ec0756519273eb62e8855497
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2019-04-04 09:09:04 +0200

    0 allocation static optim.

--- src/linesearches/backtracking.jl
+++ src/linesearches/backtracking.jl
@@ -1,11 +1,19 @@
-function backtracking(f, x::AbstractArray{T}, d, ∇f_x; α_0=T(1.0),
-                      ratio=T(0.5), c=T(0.001), ls_max_iter=50,
-                      verbose=false) where T
-    if verbose
-        println("Entering line search with step size: ", α_0)
-        println("Initial value: ", f(x))
-        println("Value at first step: ", f(x+α_0*d))
-    end
+struct LSOptions{T1, T2, T3}
+    ratio::T1
+	c::T1
+	ls_max_iter::T2
+	verbose::T3
+end
+LSOptions(;ratio=0.5, c=1e-4, ls_max_iter=100, verbose=false) = LSOptions(ratio, c, ls_max_iter, verbose)
+function backtracking(f, x, d, ∇f_x, α_0, opt)
+	ratio, c, ls_max_iter, verbose = opt.ratio, opt.c, opt.ls_max_iter, opt.verbose
+
+
+    # if verbose
+    #     println("Entering line search with step size: ", α_0)
+    #     println("Initial value: ", f(x))
+    #     println("Value at first step: ", f(x+α_0*d))
+    # end
 
 	m = dot(d, ∇f_x)
     t = -c*m
@@ -24,11 +32,11 @@ function backtracking(f, x::AbstractArray{T}, d, ∇f_x; α_0=T(1.0),
     end
 
 	ls_success = iter >= ls_max_iter ? false : true
-
-    if verbose
-		!ls_success && println("ls_max_iter exceeded in backtracking")
-        println("Exiting line search with step size: ", α)
-        println("Exiting line search with value: ", f_α)
-    end
+	#
+    # if verbose
+	# 	!ls_success && println("ls_max_iter exceeded in backtracking")
+    #     println("Exiting line search with step size: ", α)
+    #     println("Exiting line search with value: ", f_α)
+    # end
     return α, f_α, ls_success
 end
